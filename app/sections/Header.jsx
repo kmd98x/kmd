24d3283986f2a3a685e55271144b6773e6793e85
Martina Doekharan <kmd98x@hotmail.com>
2025-01-27 16:44:36 +0100

    test

--- app/sections/Header.jsx
+++ app/sections/Header.jsx
@@ -3,11 +3,16 @@ import React from "react";
 export default function Header() {
     return (
         <header>
-            <div className="header-container">
-                <div className="menu-icon"></div>
-                <div className="language-toggle">NL</div>
+            <div className="flex flex-col items-end gap-4 px-3">
+                <div className="flex flex-col gap-1.5 border border-[#FFFDD0]/50 py-2 w-12 rounded-md items-center justify-center">
+                    <span className="bg-[#FFFDD0]/50 h-px w-5 inline-block"></span>
+                    <span className="bg-[#FFFDD0]/50 h-px w-5 inline-block"></span>
+                    <span className="bg-[#FFFDD0]/50 h-px w-5 inline-block"></span>
+                    
+                </div>
+                <div className="border border-[#FFFDD0]/50 py-1.5 w-12 rounded-md text-sm text-[#FFFDD0]/50">NL</div>
             </div>
-            <div className="logo-container">
+            <div className="logo-container mb-60">
                 <img
                     className="mx-auto inline-block"
                     src="/telefoonlogo.svg"
