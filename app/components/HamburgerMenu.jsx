8fccbf3d42b6da210ec616aa79f967361cf0ae2c
Martina Doekharan <kmd98x@hotmail.com>
2025-01-27 21:00:08 +0100

    bexiggg

--- app/components/HamburgerMenu.jsx
+++ app/components/HamburgerMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function hamburgermenu({links, isOpen}) {
   return (
-    <nav className={`fixed size-full bg-black/50 backdrop-blur top-0 ${isOpen ? "left-0" : "left-[-100%]"}`}>
+    <nav className={`fixed size-full bg-black/50 backdrop-blur transition-all duration-1000 z-20 top-0 ${isOpen ? "pointer-events-auto opacity-100" : "pointer-events-none opacity-0 "}`}>
         <ul className='pt-10'>
             
             <li><a className='text-[#FFFDD0] inline-block p-5' href="">{links[0]}</a></li>
--- app/sections/Header.jsx
+++ app/sections/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <header id="home">
             <HamburgerMenu isOpen={isOpen} links={["Home", "Over mij", "Projecten", "Contact"]} />   
-            <div className="flex flex-col items-end gap-4 px-3">
+            <div className="fixed right-0 flex flex-col justify-center items-center gap-4 px-3 z-30">
                 <div className="flex flex-col gap-1.5 border border-[#FFFDD0]/50 py-2 w-12 rounded-md items-center justify-center"
                 onClick={() => setIsOpen(!isOpen)}>
                     <span className="bg-[#FFFDD0]/50 h-px w-5 inline-block"></span>
@@ -16,7 +16,7 @@ export default function Header() {
                 </div>
                 <div className="border border-[#FFFDD0]/50 py-1.5 w-12 rounded-md text-sm text-[#FFFDD0]/50">NL</div>
             </div>
-            <div className="logo-container">
+            <div className="my-auto">
                 <img
                     className="mx-auto inline-block"
                     src="/groterelogo.svg"
